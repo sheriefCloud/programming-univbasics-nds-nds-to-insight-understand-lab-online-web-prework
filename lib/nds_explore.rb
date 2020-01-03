579065357bccbbfd539c3c7cf4ea2af54330cef5
Sherief Farid <sherief@farid.cloud>
2020-01-03 05:12:37 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,10 +1,13 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require.binding.pry
 # Call the method directors_database to retrieve the NDS
 require 'pp'
+
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   puts pp nds
+  binding.pry
   pp nds
 end
 

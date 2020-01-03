77b39ba8481cc4dd260d22d0059c2ccc5d03b020
Sherief Farid <sherief@farid.cloud>
2020-01-03 05:34:53 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -7,10 +7,16 @@ require 'pp'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   puts pp nds
-  binding.pry
+  #binding.pry
   pp nds
 end
 
 def print_first_directors_movie_titles
-
+  row_index = 0
+  movies = []
+  while row_index < 1 do
+    
+  end
+    
+  end
 end

567b3e6ec635f0a5fbe9e0dec28aca62627f2bf6
Sherief Farid <sherief@farid.cloud>
2020-01-03 05:03:39 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,8 @@ require 'directors_database'
 require 'pp'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
+  puts pp nds
   pp nds
-
 end
 
 def print_first_directors_movie_titles

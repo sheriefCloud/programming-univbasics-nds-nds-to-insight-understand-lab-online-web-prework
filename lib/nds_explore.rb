04b64111ffea527a0df6c07b996870bad6931a4e
Sherief Farid <sherief@farid.cloud>
2020-01-03 04:48:35 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,14 +4,34 @@ require 'directors_database'
 require 'pp'
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  a= pp(nds)
-  pp a_o_a
-  pp a_privateer
-pp an_integer
-pp a_o_a
-pp vm
-puts a
+  pp nds
+
 end
 
 def print_first_directors_movie_titles
+#   row_index = 0
+#   while row_index < nds.length do
+#   puts "Row #{row_index} has #{vm[row_index]} columns"
+#  
+#   column_index = 0
+#   while column_index < vm[row_index].length do
+#     coord = "#{row_index}, #{column_index}"
+#     inner_len = vm[row_index][column_index].length
+#     # Remember \t is a TAB character for indentation
+#     puts "\tCoordinate [#{coord}] points to an #{vm[row_index][column_index].class} of length #{inner_len}"
+#  
+#     inner_index = 0
+#     while inner_index < inner_len do
+#       puts "\t\t (#{coord}, #{inner_index}) is: #{vm[row_index][column_index][inner_index]}"
+#       inner_index += 1
+#     end
+#  
+#     column_index += 1
+#   end
+#  
+#   row_index += 1
+# end
+#     
+#   end
+  
 end
